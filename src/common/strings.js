70afa41cda9a282b11c3336aec5b053dcfc34993
jevvilla <juan.es-19@hotmail.com>
2019-11-30 11:13:05 -0500

    strings and styles file added

---
+++ src/common/strings.js
@@ -0,0 +1,7 @@
+// LOGIN
+export const LOGIN_TITLE = 'Welcome to resataurant';
+export const LOGIN_SUBTITLE = 'Please Login';
+export const LOGIN_BUTTON_TEXT = 'Login';
+export const EMAIL_PLACEHOLDER = 'Insert your email';
+export const PASSWORD_PLACEHOLDER = 'Insert your password';
+export const LOGIN_ERROR_MESSAGE = 'User cannot be found 😔';
