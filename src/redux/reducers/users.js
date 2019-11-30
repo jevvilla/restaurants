2f47990b91ae5bd1d6211611421db6889fdb28ac
jevvilla <juan.es-19@hotmail.com>
2019-11-30 11:14:30 -0500

    user reducer implemented

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -8,13 +8,27 @@ const INITIAL_STATE = {
       password: '0000',
     },
   ],
+  userLogged: null,
   history: [],
+};
+
+const findUser = (action, state) => {
+  const { email, password } = action.payload;
+
+  return state.users.find(user => {
+    if (user.email === email && user.password === password) {
+      return user;
+    }
+    return null;
+  });
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case types.GET_USER_BY_EMAIL:
-      return state;
+    case types.GET_USER_BY_EMAIL_AND_PASSWORD:
+      // eslint-disable-next-line no-case-declarations
+      const userLogged = findUser(action, state);
+      return { ...state, userLogged };
 
     case types.REGISTER_USER:
       return state;
