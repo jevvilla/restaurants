70e663296c964c369d8acbfee12bb8dbc1dcdf57
jevvilla <juan.es-19@hotmail.com>
2019-11-30 09:28:09 -0500

    routes modified, some extra routes were added

--- src/common/navigation/routes.js
+++ src/common/navigation/routes.js
@@ -1,2 +1,11 @@
-export const HOME = 'Home';
-export const HISTORY = 'History';
+// Main Switch Navigator
+export const SWITCH_APP = 'SWITCH_APP';
+export const SWITCH_LOGIN = 'SWITCH_LOGIN';
+
+// Login
+export const LOGIN = 'LOGIN';
+export const REGISTER = 'REGISTER';
+
+// Main Screens
+export const HOME = 'HOME';
+export const HISTORY = 'HISTORY';
