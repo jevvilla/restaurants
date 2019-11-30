ed5e406f7518861f5c6c0cc5b76643985f4a4606
jevvilla <juan.es-19@hotmail.com>
2019-11-30 12:55:16 -0500

    string commons and constants file were updated with new data

--- src/common/strings.js
+++ src/common/strings.js
@@ -5,3 +5,8 @@ export const LOGIN_BUTTON_TEXT = 'Login';
 export const EMAIL_PLACEHOLDER = 'Insert your email';
 export const PASSWORD_PLACEHOLDER = 'Insert your password';
 export const LOGIN_ERROR_MESSAGE = 'User cannot be found 😔';
+
+// HOME
+export const SEARCH_CITY_PLACEHOLDER = 'Insert a city name';
+export const SEARCH_CITY_BUTTON_TEXT = 'Search city';
+export const HOME_TITLE = 'Search restaurants near me!';
